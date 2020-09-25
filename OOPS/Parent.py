24acc94a1bfa73a6af715bf7514578c7e1ca7ae8
SarangIntellify <sarang.t@theintellify.com>
2020-09-26 01:53:30 +0530

    Inheritance

---
+++ OOPS/Parent.py
@@ -0,0 +1,35 @@
+class Parent:
+
+    def featureOne(self):
+        print("Feature one of parent")
+    
+    def featureTwo(self):
+        print("Feature two of parent")
+
+class Child(Parent):  #inheriting from parent class
+
+        def featureThree(self):
+            print("Feature Three of child")
+
+        def featureFour(self):
+            print("Feature Four of child")
+
+class SmallChild(Child): #multi level inheritance
+
+        def featureFive(self):
+            print("Feature Five of Small Child")
+
+class Others:
+
+        def featureFive(self):
+            print("Feature of others")
+
+#if inherited class have same method name then class you write first inside parenthesis, that class method will be considered
+class GrandParent(Others, SmallChild): #multiple inheritance
+
+        def featureSix(self):
+            print("Feature six of grandparent")
+
+
+g = GrandParent()
+g.featureFive()
