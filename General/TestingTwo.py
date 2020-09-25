a8c9f40089a2f0fd96dfb1138f36205de5b3fc5a
SarangIntellify <sarang.t@theintellify.com>
2020-09-26 00:50:22 +0530

    static variables

--- General/TestingTwo.py
+++ General/TestingTwo.py
@@ -1,2 +1,24 @@
 class TestingTwo:
           
+        college = "SOCET" #static variable
+
+        def __init__(self, name, age, animal):
+            print("Constructor called")
+            self.myName = name
+            self.myAge = age
+            self.myAnimal = animal
+
+        def getName(self):
+            return self.myName
+        
+        def getAge(self):
+            return self.myAge
+
+        def getAnimal(self):
+            return self.myAnimal
+
+
+t = TestingTwo("Sarang", 12, "giraffe")
+print(t.getAge(), TestingTwo.college)
+print(t.getName(), TestingTwo.college)
+print(t.getAnimal(), TestingTwo.college)
