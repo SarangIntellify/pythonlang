674bad8f2e4b3c6331bd6fe7723ce754ab786b2f
SarangIntellify <sarang.t@theintellify.com>
2020-09-28 18:32:22 +0530

    parameters in request url

--- FlaskRest/Test.py
+++ FlaskRest/Test.py
@@ -4,5 +4,5 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 #sending the get request to base url + helloworld
-response = requests.get(BASE + "helloworld")
+response = requests.post(BASE + "helloworld/sarang/15")
 print(response.json())
--- FlaskRest/main.py
+++ FlaskRest/main.py
@@ -6,10 +6,13 @@ api = Api(app) #initializing the rest api module using Api class.
 
 #resource class must be child of Resource class so we can override the method of parent Resource class.
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Helloworld"}
+    def get(self, name, test): #accessing the variable from request url
+        return {"name": name, "test":test}
     
-api.add_resource(HelloWorld, "/helloworld") 
+    def post(self):
+        return {"data": "Helloworld from post"}
+    
+api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>") #parameters in request url
 
 if __name__ == "__main__":
     app.run(debug = True)
